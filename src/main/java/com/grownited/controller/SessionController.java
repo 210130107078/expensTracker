4caaecdd9da3fef8182d65dcd766abb0579fef3e
210130107078 <210130107078@gecg28.ac.in>
2025-02-26 13:53:40 +0530

    Session

--- src/main/java/com/grownited/controller/SessionController.java
+++ src/main/java/com/grownited/controller/SessionController.java
@@ -14,6 +14,8 @@ import com.grownited.entity.UserEntity;
 import com.grownited.repository.UserRepository;
 import com.grownited.service.MailService;
 
+import jakarta.servlet.http.HttpSession;
+
 
 //Get Post 
 
@@ -57,7 +59,7 @@ public class SessionController {
 	}
 	
 	@PostMapping("authenticate")
-	public String authenticate(String email, String password,Model model) {// sakira@yopmail.com sakira
+	public String authenticate(String email, String password,Model model,HttpSession session) {// sakira@yopmail.com sakira
 		System.out.println(email);
 		System.out.println(password);
 
@@ -68,15 +70,35 @@ public class SessionController {
 			// true
 			// email
 			UserEntity dbUser = op.get();
-			if (encoder.matches(password, dbUser.getPassword())) {
-				return "redirect:/home";
+			
+			boolean ans = encoder.matches(password, dbUser.getPassword());
+
+			if (ans == true) {
+				session.setAttribute("user", dbUser); // session -> user set
+				if (dbUser.getRole().equals("ADMIN")) {
+
+					return "redirect:/admindashboard";
+				} else if (dbUser.getRole().equals("USER")) {
+
+					return "redirect:/home";
+				} else {
+					model.addAttribute("error", "Please contact Admin with Error Code #2222");
+					return "Login";
+				}
+
 			}
 		}
-		model.addAttribute("error","Invalid Credentials");
+		model.addAttribute("error", "Invalid Credentials");
 		return "Login";
+
+	}
+	
+	@GetMapping("logout")
+	public String logout(HttpSession session) {
+		session.invalidate();
+		return "redirect:/login";// login url
 	}
 
-	
 	
 
 	// open ForgetPassword jsp
